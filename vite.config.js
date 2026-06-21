d9ce116d12cc7903bd1b3c99b98ccc749fc9870d
itskendel <itskendelantar@gmail.com>
2026-06-21 11:49:20 +0800

    fix url

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   base: '/scoreboard_pickleball/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        index: '/index.html',
+        score: '/score.html',
+        result: '/result.html',
+      },
+    },
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
